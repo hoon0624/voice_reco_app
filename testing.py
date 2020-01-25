42e7a881c1fbcca0878435bc8df88117086aa4fa
Jae Choi <ladderjy@gmail.com>
2020-01-25 11:09:34 -0500

    updated tesitng

--- testing.py
+++ testing.py
@@ -1,2 +1,21 @@
-def testting(i):
-    return i
+from flask import Flask, request, jsonify
+from speech_utils import Recorder, Recording
+from speech_recognition import recognizer
+
+# initialize Flask app
+@app.route('/')
+app = Flask(__name__)
+
+
+if __name__ == '__main__': 
+    app.run(debug = True)
+
+def outcome(filepath):
+    if(request.method == 'GET'):
+        recorder = Recorder()
+        recording = Recording(recognizer, filepath)
+        print(recording)
+
+        return recording
+
+
